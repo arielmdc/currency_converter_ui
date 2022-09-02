04f2bb22c848f6c535818a0f38cef8ee25e4d56c
Ariel Mota <arielmcarvalho@gmail.com>
2022-09-02 10:18:00 -0300

    feat: dashboard page

---
+++ currency_converter_ui/src/pages/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import FormComponent from '../components/form/FormComponent';
+
+const Dashboard = () => (
+  <>
+    <FormComponent />
+  </>
+);
+
+export default Dashboard;
