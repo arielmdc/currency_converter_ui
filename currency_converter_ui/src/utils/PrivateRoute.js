2970e98ddc9becce5dedb1de0d2b61ec1b279226
Ariel Mota <arielmcarvalho@gmail.com>
2022-09-02 09:58:40 -0300

    feat: implement private route

---
+++ currency_converter_ui/src/utils/PrivateRoute.js
@@ -0,0 +1,29 @@
+import React, { useContext } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import { AuthContext } from '../contexts/AuthContext.js';
+
+import LayoutBase from '../components/layoutBaseComponents/LayoutBase';
+import Preloader from '../components/Preloader';
+
+function PrivateRoute(props) {
+  const { component, ...rest } = props;
+  const { loadingAuth, isAuthenticated } = useContext(AuthContext);
+
+  if (loadingAuth) return <Preloader />;
+
+  if (!isAuthenticated) {
+    if (rest.location.pathname && rest.location.pathname !== '/') {
+      return (
+        <Redirect
+          to={`/login?redirect=${
+            rest.location.pathname + rest.location.search
+          }`}
+        />
+      );
+    } else return <Redirect to={`/login`} />;
+  } else {
+    return <Route {...rest} render={() => <LayoutBase Page={component} />} />;
+  }
+}
+
+export default PrivateRoute;
